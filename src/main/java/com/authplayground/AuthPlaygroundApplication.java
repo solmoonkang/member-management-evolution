4fe4480ac5db9ad9729de55325c158ce7c269f32
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-20 18:35:43 +0900

    초기 설정

---
+++ src/main/java/com/authplayground/AuthPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.authplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authplayground/AuthPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
