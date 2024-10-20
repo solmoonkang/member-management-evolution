4c8a6c9934608e380710285628d85b5dda123bd1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-20 19:09:01 +0900

    feat: 회원 모델 생성 및 회원가입 로직 구현

---
+++ src/main/java/com/authplayground/api/domain/member/Member.java
@@ -0,0 +1,50 @@
+package com.authplayground.api.domain.member;
+
+import com.authplayground.api.dto.member.SignUpRequest;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id")
+	private Long id;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "nickname")
+	private String nickname;
+
+	@Column(name = "password")
+	private String password;
+
+	@Builder
+	private Member(String email, String nickname, String password) {
+		this.email = email;
+		this.nickname = nickname;
+		this.password = password;
+	}
+
+	public static Member createMember(SignUpRequest signUpRequest, String password) {
+		return Member.builder()
+			.email(signUpRequest.email())
+			.nickname(signUpRequest.nickname())
+			.password(password)
+			.build();
+	}
+}
