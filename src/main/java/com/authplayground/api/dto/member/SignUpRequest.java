776593bcf8d1465dc706f9df8aabd3357f852438
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-20 19:09:41 +0900

    feat: 회원가입 DTO 생성 및 유효성 검사

---
+++ src/main/java/com/authplayground/api/dto/member/SignUpRequest.java
@@ -0,0 +1,22 @@
+package com.authplayground.api.dto.member;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record SignUpRequest(
+	@Email(message = "이메일 형식에 맞도록 작성해주세요.")
+	@NotBlank(message = "이메일을 입력해주세요.")
+	String email,
+
+	@NotBlank(message = "닉네임을 입력해주세요.")
+	String nickname,
+
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	String password,
+
+	@NotBlank(message = "확인 비밀번호를 입력해주세요.")
+	String checkPassword
+) {
+}
