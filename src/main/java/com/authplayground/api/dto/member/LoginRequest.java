9b79bea16a4c41f32e2324ecc8da5db0b60e74e2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-22 03:35:29 +0900

    feat: 로그인 요청 DTO 구현

---
+++ src/main/java/com/authplayground/api/dto/member/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.authplayground.api.dto.member;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record LoginRequest(
+	@NotBlank(message = "이메일을 입력해주세요.")
+	String email,
+
+	@NotBlank(message = "비밀번호를 입력해주세요.")
+	String password
+) {
+}
