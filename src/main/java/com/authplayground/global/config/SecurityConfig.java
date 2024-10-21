b7f4b0b730c074c8d7be8cfb91c24d5360efffa2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-21 15:37:44 +0900

    feat: 스프링 시큐리티 필터 체인 및 비밀번호 암호화 설정

---
+++ src/main/java/com/authplayground/global/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.authplayground.global.config;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.*;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return webSecurityCustomizer -> webSecurityCustomizer.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+			.requestMatchers("/h2-console/**");
+	}
+
+	@Bean
+	protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable);
+
+		httpSecurity.authorizeHttpRequests(auth -> auth
+			.requestMatchers("/", "/api/signup", "/api/login").permitAll()
+			.anyRequest().authenticated());
+
+		httpSecurity.logout(logout -> logout
+			.logoutSuccessUrl("/api/login")
+			.invalidateHttpSession(true));
+
+		httpSecurity.sessionManagement(session -> session
+			.sessionCreationPolicy(STATELESS));
+
+		return httpSecurity.build();
+	}
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+}
