3a47ea86487f84f33c9168c04dbbdd28077b49fa
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-22 14:24:57 +0900

    refactor: 비밀번호 일치 검사 로직 수정

--- src/main/java/com/authplayground/api/application/MemberService.java
+++ src/main/java/com/authplayground/api/application/MemberService.java
@@ -42,7 +42,7 @@ public class MemberService {
 	}
 
 	private void validatePasswordMatch(String password, String checkPassword) {
-		if (password.equals(checkPassword)) {
+		if (!password.equals(checkPassword)) {
 			throw new IllegalArgumentException("[❎ ERROR] 입력하신 비밀번호와 일치하지 않습니다.");
 		}
 	}
