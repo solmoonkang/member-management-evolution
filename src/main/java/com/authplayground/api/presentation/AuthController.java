a36604852fbea58c1758619ddc8cfad72c0dae88
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-24 15:52:15 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/com/authplayground/api/presentation/AuthController.java
+++ src/main/java/com/authplayground/api/presentation/AuthController.java
@@ -2,10 +2,6 @@ package com.authplayground.api.presentation;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
