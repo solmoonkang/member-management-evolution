f674008d074a5c531eada337ff50829ab98eb80f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-25 17:32:28 +0900

    feat: 찾을 수 없는 리소스 처리 예외 클래스 구현

---
+++ src/main/java/com/authplayground/global/error/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.authplayground.global.error.exception;
+
+public class NotFoundException extends AuthPlaygroundException {
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
