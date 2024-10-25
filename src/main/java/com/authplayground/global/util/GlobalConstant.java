15c45ae8af78c83500228d375833f726eeecfe02
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-25 17:31:06 +0900

    feat: 전역 상수 선언 클래스 구현

---
+++ src/main/java/com/authplayground/global/util/GlobalConstant.java
@@ -0,0 +1,20 @@
+package com.authplayground.global.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GlobalConstant {
+
+	// JWT 클레임 상수
+	public static final String MEMBER_EMAIL = "email";
+	public static final String MEMBER_NICKNAME = "nickname";
+
+	// JWT 관련 상수
+	public static final String BLANK = " ";
+	public static final String BEARER = "Bearer";
+
+	// 액세스 토큰 & 리프레시 토큰 헤더 및 쿠키 상수
+	public static final String ACCESS_TOKEN_HEADER = "Authorization";
+	public static final String REFRESH_TOKEN_COOKIE = "Authorization_RefreshToken";
+}
