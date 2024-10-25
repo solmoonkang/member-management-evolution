e8506406469dee0caf56982570e86046abcab98f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-25 17:32:33 +0900

    feat: 충돌 처리 예외 클래스 구현

---
+++ src/main/java/com/authplayground/global/error/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package com.authplayground.global.error.exception;
+
+public class ConflictException extends AuthPlaygroundException {
+
+	public ConflictException(String message) {
+		super(message);
+	}
+}
