5e9f6321d69f4e3d9cbfa206a5069f9d31f62264
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-10-25 17:31:19 +0900

    feat: 비즈니스 로직 예외 처리 클래스 구현

---
+++ src/main/java/com/authplayground/global/error/exception/AuthPlaygroundException.java
@@ -0,0 +1,8 @@
+package com.authplayground.global.error.exception;
+
+public class AuthPlaygroundException extends RuntimeException {
+
+	public AuthPlaygroundException(String message) {
+		super(message);
+	}
+}
