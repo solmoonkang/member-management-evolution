ad558b82ed688d95a83a411942da25f5668cba71
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-15 22:40:04 +0900

    refactor: 에러 응답 메시지를 사용한 예외 처리를 하도록 수정

--- src/main/java/com/authplayground/api/domain/auth/repository/HashRedisRepository.java
+++ src/main/java/com/authplayground/api/domain/auth/repository/HashRedisRepository.java
@@ -1,5 +1,7 @@
 package com.authplayground.api.domain.auth.repository;
 
+import static com.authplayground.global.error.model.ErrorMessage.*;
+
 import java.time.Duration;
 import java.util.Map;
 
@@ -8,17 +10,19 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.hash.Jackson2HashMapper;
 import org.springframework.stereotype.Repository;
 
-import com.authplayground.api.dto.auth.TokenRequest;
-import com.authplayground.global.error.exception.ConflictException;
-
-import lombok.RequiredArgsConstructor;
+import com.authplayground.api.dto.response.TokenResponse;
+import com.authplayground.global.error.exception.UnauthorizedException;
 
 @Repository
-@RequiredArgsConstructor
 public class HashRedisRepository {
 
 	private final RedisTemplate<String, Object> redisTemplate;
 	private final HashOperations<String, String, Object> hashOperations;
+
+	public HashRedisRepository(RedisTemplate<String, Object> redisTemplate) {
+		this.redisTemplate = redisTemplate;
+		this.hashOperations = redisTemplate.opsForHash();
+	}
 
 	public void save(String key, Object value, Duration timeout) {
 		hashOperations.putAll(key, new Jackson2HashMapper(false).toHash(value));
@@ -29,7 +33,7 @@ public class HashRedisRepository {
 		final Map<String, Object> memberTokenMap = hashOperations.entries(key);
 		validUnauthorizedMember(memberTokenMap);
 
-		return TokenRequest.createTokenFromMap(memberTokenMap);
+		return TokenResponse.createTokenFromMap(memberTokenMap);
 	}
 
 	public void delete(String key) {
@@ -39,7 +43,7 @@ public class HashRedisRepository {
 	// TODO: SRP(단일 책임 원칙) 위배 가능성이 있으므로, 서비스에서 비즈니스 로직을 처리할 때 검증을 할 수 있도록 하자.
 	private void validUnauthorizedMember(Map<String, Object> memberTokenMap) {
 		if (memberTokenMap.isEmpty()) {
-			throw new ConflictException("[❎ ERROR] 해당 사용자는 인증되지 않은 사용자입니다.");
+			throw new UnauthorizedException(FAILED_UNAUTHORIZED_MEMBER);
 		}
 	}
 }
