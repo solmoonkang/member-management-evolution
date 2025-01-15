6df8540e1a458cf603af64e17d4bad73a4661147
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-15 20:31:33 +0900

    refactor: 에러 응답 메시지를 반환하도록 수정

--- src/main/java/com/authplayground/global/error/exception/ConflictException.java
+++ src/main/java/com/authplayground/global/error/exception/ConflictException.java
@@ -1,8 +1,10 @@
 package com.authplayground.global.error.exception;
 
+import com.authplayground.global.error.model.ErrorMessage;
+
 public class ConflictException extends AuthPlaygroundException {
 
-	public ConflictException(String message) {
-		super(message);
+	public ConflictException(ErrorMessage errorMessage) {
+		super(errorMessage.getMessage());
 	}
 }
