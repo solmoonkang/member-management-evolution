db2bafb806943e9bfbe2797a380e4551234a17d2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 17:05:58 +0900

    feat: 회원 모델에 역할 필드 추가

--- src/main/java/com/authplayground/api/domain/member/Member.java
+++ src/main/java/com/authplayground/api/domain/member/Member.java
@@ -1,9 +1,14 @@
 package com.authplayground.api.domain.member;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import com.authplayground.api.dto.request.SignUpRequest;
+import com.authplayground.global.common.entity.BaseTimeEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -15,32 +20,35 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Table(name = "tbl_members")
+@Table(name = "MEMBER")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "member_id")
 	private Long id;
 
-	@Column(name = "email")
+	@Column(name = "email", nullable = false, unique = true)
 	private String email;
 
-	@Column(name = "nickname")
+	@Column(name = "nickname", unique = true)
 	private String nickname;
 
 	@Column(name = "password")
 	private String password;
 
-	@Column(name = "refresh_token")
-	private String refreshToken;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false)
+	@ColumnDefault("'MEMBER'")
+	private Role role;
 
 	@Builder
 	private Member(String email, String nickname, String password) {
 		this.email = email;
 		this.nickname = nickname;
 		this.password = password;
+		this.role = Role.MEMBER;
 	}
 
 	public static Member createMember(SignUpRequest signUpRequest, String password) {
@@ -49,9 +57,5 @@ public class Member {
 			.nickname(signUpRequest.nickname())
 			.password(password)
 			.build();
-	}
-
-	public void updateMemberRefreshToken(String refreshToken) {
-		this.refreshToken = refreshToken;
 	}
 }
