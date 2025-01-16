6428c427f5bb24de2c76148ec6c8a42a5a24f239
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 17:06:39 +0900

    feat: 역할 필드 추가

--- src/main/java/com/authplayground/api/domain/auth/AuthMember.java
+++ src/main/java/com/authplayground/api/domain/auth/AuthMember.java
@@ -2,13 +2,19 @@ package com.authplayground.api.domain.auth;
 
 import java.util.Objects;
 
+import com.authplayground.api.domain.member.Role;
+
 public record AuthMember(
 	String email,
-
-	String nickname
+	String nickname,
+	Role role
 ) {
 
-	public static AuthMember createAuthMember(String email, String nickname) {
-		return new AuthMember(Objects.requireNonNull(email), Objects.requireNonNull(nickname));
+	public static AuthMember createAuthMember(String email, String nickname, Role role) {
+		return new AuthMember(
+			Objects.requireNonNull(email),
+			Objects.requireNonNull(nickname),
+			Objects.requireNonNull(role)
+		);
 	}
 }
