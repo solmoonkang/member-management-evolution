362a5759da3bd0e4321ee45c0a236eed228ac002
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 14:15:18 +0900

    feat: 토큰 설정 프로퍼티 클래스를 스프링 빈으로 등록

--- src/main/java/com/authplayground/AuthPlaygroundApplication.java
+++ src/main/java/com/authplayground/AuthPlaygroundApplication.java
@@ -2,8 +2,12 @@ package com.authplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.authplayground.global.config.TokenConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(TokenConfig.class)
 public class AuthPlaygroundApplication {
 
 	public static void main(String[] args) {
