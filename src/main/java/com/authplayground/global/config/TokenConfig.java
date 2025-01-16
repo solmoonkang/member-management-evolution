222243ff27b466739f56bf74f588ecbbc0d2cd30
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 14:13:58 +0900

    refactor: 시크릿 키 생성 및 생성자 주입을 하도록 수정

--- src/main/java/com/authplayground/global/config/TokenConfig.java
+++ src/main/java/com/authplayground/global/config/TokenConfig.java
@@ -1,17 +1,32 @@
 package com.authplayground.global.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 
-@Component
 @Getter
 @ConfigurationProperties(prefix = "jwt")
 public class TokenConfig {
 
-	private String iss;
-	private String secretAccessKey;
-	private long accessTokenExpire;
-	private long refreshTokenExpire;
+	private final String iss;
+	private final String secretAccessKey;
+	private final long accessTokenExpire;
+	private final long refreshTokenExpire;
+	private final SecretKey secretKey;
+
+	@ConstructorBinding
+	public TokenConfig(String iss, String secretAccessKey, long accessTokenExpire, long refreshTokenExpire) {
+		this.iss = iss;
+		this.secretAccessKey = secretAccessKey;
+		this.accessTokenExpire = accessTokenExpire;
+		this.refreshTokenExpire = refreshTokenExpire;
+
+		byte[] secretKeyBytes = Decoders.BASE64.decode(secretAccessKey);
+		this.secretKey = Keys.hmacShaKeyFor(secretKeyBytes);
+	}
 }
