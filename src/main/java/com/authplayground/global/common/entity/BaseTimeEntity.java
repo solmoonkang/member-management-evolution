c158bc7ddcaa3bc0543ea55c1e44f857c574ae34
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 17:05:30 +0900

    feat: BaseTimeEntity 구현

---
+++ src/main/java/com/authplayground/global/common/entity/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.authplayground.global.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
