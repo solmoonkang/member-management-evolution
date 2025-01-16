f848c5ca25d0c9f5e82431bd17757a81d3fcd873
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 17:23:56 +0900

    feat: 리프레시 토큰 레디스 키 값 추가

---
+++ src/main/java/com/authplayground/global/common/util/RedisConstant.java
@@ -0,0 +1,10 @@
+package com.authplayground.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RedisConstant {
+
+	public static final String REDIS_REFRESH_TOKEN_PREFIX = "refreshToken";
+}
