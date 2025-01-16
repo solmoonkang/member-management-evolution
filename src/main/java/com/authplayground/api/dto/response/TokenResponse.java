be43afaec8b1e67ae14141030b77d0989d3dadf1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-16 17:24:22 +0900

    feat: 리프레시 토큰 키 값 추가

--- src/main/java/com/authplayground/api/dto/response/TokenResponse.java
+++ src/main/java/com/authplayground/api/dto/response/TokenResponse.java
@@ -1,5 +1,7 @@
 package com.authplayground.api.dto.response;
 
+import static com.authplayground.global.common.util.RedisConstant.*;
+
 import java.util.Map;
 
 import lombok.Builder;
@@ -9,10 +11,9 @@ public record TokenResponse(
 	String refreshToken
 ) {
 
-	// TODO: Map에 refreshToken으로 할당하는 이유
 	public static TokenResponse createTokenFromMap(Map<String, Object> memberTokenMap) {
 		return TokenResponse.builder()
-			.refreshToken((String)memberTokenMap.get("refreshToken"))
+			.refreshToken((String)memberTokenMap.get(REDIS_REFRESH_TOKEN_PREFIX))
 			.build();
 	}
 }
