812f06fe52d50073899725344f46d9a57da25a71
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-17 18:34:12 +0900

    feat: BaseTimeEntity 활성화

--- src/main/java/com/authplayground/AuthPlaygroundApplication.java
+++ src/main/java/com/authplayground/AuthPlaygroundApplication.java
@@ -3,10 +3,12 @@ package com.authplayground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.authplayground.global.config.TokenConfig;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableConfigurationProperties(TokenConfig.class)
 public class AuthPlaygroundApplication {
 
