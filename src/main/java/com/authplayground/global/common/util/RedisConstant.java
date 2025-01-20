026e6fc3deed30de4d989cfb420edacd6111af46
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 14:24:38 +0900

    feat: Redis 세션 키 값 추가

--- src/main/java/com/authplayground/global/common/util/RedisConstant.java
+++ src/main/java/com/authplayground/global/common/util/RedisConstant.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 public class RedisConstant {
 
 	public static final String REDIS_REFRESH_TOKEN_PREFIX = "refreshToken";
+	public static final String REDIS_SESSION_PREFIX = "session";
 
 	public static final int EXPIRE_DAYS = 14;
 }
