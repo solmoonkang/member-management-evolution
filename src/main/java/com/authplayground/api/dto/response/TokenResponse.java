bbc691a1933ba024790d8c2aaf82643f4252fe1c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-01-20 14:24:26 +0900

    refactor: 불필요한 로직 제거

--- src/main/java/com/authplayground/api/dto/response/TokenResponse.java
+++ src/main/java/com/authplayground/api/dto/response/TokenResponse.java
@@ -1,19 +1,9 @@
 package com.authplayground.api.dto.response;
 
-import static com.authplayground.global.common.util.RedisConstant.*;
-
-import java.util.Map;
-
 import lombok.Builder;
 
 @Builder
 public record TokenResponse(
 	String refreshToken
 ) {
-
-	public static TokenResponse createTokenFromMap(Map<String, Object> memberTokenMap) {
-		return TokenResponse.builder()
-			.refreshToken((String)memberTokenMap.get(REDIS_REFRESH_TOKEN_PREFIX))
-			.build();
-	}
 }
