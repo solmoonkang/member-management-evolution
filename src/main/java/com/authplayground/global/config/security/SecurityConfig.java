a08296dc7db6bf3cd2faae57f637e7e61ef289a9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-25 19:51:43 +0900

    feat: 시큐리티 설정 클래스 구현

---
+++ src/main/java/com/authplayground/global/config/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.authplayground.global.config.security;
+
+import static com.authplayground.global.common.util.AuthConstant.*;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.authplayground.api.domain.member.model.Role;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
+	public SecurityConfig(CustomAuthenticationEntryPoint customAuthenticationEntryPoint) {
+		this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return webSecurity -> webSecurity.ignoring()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+			.requestMatchers(SECURITY_IGNORED_URLS);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable);
+
+		httpSecurity.authorizeHttpRequests(auth -> auth
+			.requestMatchers(PUBLIC_API_PATHS).permitAll()
+			.requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
+			.anyRequest().authenticated());
+
+		httpSecurity.sessionManagement(session -> session
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+		httpSecurity.exceptionHandling(exceptionHandling ->
+			exceptionHandling.authenticationEntryPoint(customAuthenticationEntryPoint));
+
+		return httpSecurity.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+		throws Exception {
+
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+}
