910a68767a61bb45a463613d18edd3506bd06bf5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-25 19:51:07 +0900

    feat: 시큐리티 요청 무시 및 화이트리스트 URL 상수 정의

---
+++ src/main/java/com/authplayground/global/common/util/AuthConstant.java
@@ -0,0 +1,19 @@
+package com.authplayground.global.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuthConstant {
+
+	public static final String[] SECURITY_IGNORED_URLS = {
+		"/h2-console/**",
+		"/swagger-ui/**",
+		"/swagger-resources/**",
+		"/v3/api-docs/**"
+	};
+
+	public static final String[] PUBLIC_API_PATHS = {
+		"/api/signup", "/api/login"
+	};
+}
