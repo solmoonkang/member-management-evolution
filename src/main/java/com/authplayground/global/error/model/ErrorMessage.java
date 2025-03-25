8add0c8562f695f1d4380d828c9b3622b34b1ebb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-25 18:14:06 +0900

    feat: AES 암복호화 에러 메시지 정의

--- src/main/java/com/authplayground/global/error/model/ErrorMessage.java
+++ src/main/java/com/authplayground/global/error/model/ErrorMessage.java
@@ -8,16 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorMessage {
 
-	FAILED_INVALID_PASSWORD("[❎ ERROR] 입력하신 비밀번호는 틀린 비밀번호입니다."),
-	FAILED_INVALID_TOKEN("[❎ ERROR] 유효하지 않은 토큰입니다."),
-	FAILED_UNAUTHORIZED_MEMBER("[❎ ERROR] 해당 사용자는 인증되지 않은 사용자입니다."),
-	FAILED_EMAIL_DUPLICATION("[❎ ERROR] 입력하신 이메일은 이미 존재하는 이메일입니다."),
-	FAILED_NICKNAME_DUPLICATION("[❎ ERROR] 입력하신 닉네임은 이미 존재하는 닉네임입니다."),
-	FAILED_PASSWORD_MISMATCH("[❎ ERROR] 입력하신 비밀번호와 일치하지 않습니다."),
-	FAILED_MEMBER_NOT_FOUND("[❎ ERROR] 요청하신 사용자는 존재하지 않는 사용자입니다."),
-	FAILED_TOKEN_NOT_FOUND("[❎ ERROR] JWT 토큰이 존재하지 않습니다."),
-
-	FAILED_UNKNOWN_ERROR("[❎ ERROR] 서버에서 알 수 없는 에러가 발생했습니다.");
+	FAILED_AES_INIT_FAILURE("[❎ ERROR] AES 초기화 중 예외가 발생했습니다."),
+	FAILED_AES_ENCRYPT_FAILURE("[❎ ERROR] AES 암호화 중 예외가 발생했습니다."),
+	FAILED_AES_DECRYPT_FAILURE("[❎ ERROR] AES 복호화 중 예외가 발생했습니다.");
 
 	private final String message;
 }
