b18a733f4897fc90c8dc5d8bedd5efa878ae6053
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-25 18:13:03 +0900

    chore: 불필요한 import문 제거

--- src/main/java/com/authplayground/AuthPlaygroundApplication.java
+++ src/main/java/com/authplayground/AuthPlaygroundApplication.java
@@ -2,14 +2,8 @@ package com.authplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-import com.authplayground.global.config.TokenConfig;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableConfigurationProperties(TokenConfig.class)
 public class AuthPlaygroundApplication {
 
 	public static void main(String[] args) {
