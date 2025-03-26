d17779409ccf12982fe523c473867b7e4e174ab5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-26 18:15:04 +0900

    feat: 사용자 정보 삭제 API 엔드포인트 설정

--- src/main/java/com/authplayground/api/presentation/MemberController.java
+++ src/main/java/com/authplayground/api/presentation/MemberController.java
@@ -2,13 +2,16 @@ package com.authplayground.api.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.authplayground.api.application.auth.AuthenticationService;
 import com.authplayground.api.application.member.MemberService;
 import com.authplayground.api.domain.auth.CustomUserDetails;
 import com.authplayground.api.dto.member.request.LoginRequest;
@@ -25,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberController {
 
+	private final AuthenticationService authenticationService;
 	private final MemberService memberService;
 
 	@PostMapping("/signup")
@@ -38,7 +42,7 @@ public class MemberController {
 		@RequestBody @Valid LoginRequest loginRequest,
 		HttpServletRequest httpServletRequest) {
 
-		memberService.loginMember(loginRequest, httpServletRequest);
+		authenticationService.loginMember(loginRequest, httpServletRequest);
 		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 인증을 성공적으로 완료했습니다.");
 	}
 
@@ -47,12 +51,18 @@ public class MemberController {
 		return ResponseEntity.ok().body(memberService.findMemberInfo(userDetails.member().getId()));
 	}
 
-	@PutMapping("/update")
+	@PutMapping("/members/{memberId}/update")
 	public ResponseEntity<String> updateMember(
-		@AuthenticationPrincipal CustomUserDetails userDetails,
+		@PathVariable Long memberId,
 		@RequestBody @Valid UpdateRequest updateRequest) {
 
-		memberService.updateMember(userDetails.member().getId(), updateRequest);
+		memberService.updateMember(memberId, updateRequest);
 		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 정보 수정을 성공적으로 완료했습니다.");
+	}
+
+	@DeleteMapping("/members/{memberId}")
+	public ResponseEntity<String> deleteMember(@PathVariable Long memberId) {
+		memberService.deleteMember(memberId);
+		return ResponseEntity.ok().body("[✅ SUCCESS] 사용자 정보 삭제를 성공적으로 완료했습니다.");
 	}
 }
