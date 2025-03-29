6808fbe065305c0489682d461d4058986c81fd68
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-30 01:26:13 +0900

    feat: 유효하지 않은 인가 헤더 에러 메시지 정의

--- src/main/java/com/authplayground/global/error/model/ErrorMessage.java
+++ src/main/java/com/authplayground/global/error/model/ErrorMessage.java
@@ -13,6 +13,7 @@ public enum ErrorMessage {
 
 	// 401 Unauthorized
 	UNAUTHORIZED_REQUEST("[❎ ERROR] 인증되지 않은 사용자 요청입니다."),
+	INVALID_AUTHORIZATION_HEADER("[❎ ERROR] 유효하지 않은 Authorization 헤더입니다."),
 
 	// 403 Forbidden
 	NO_PERMISSION_FAILURE("[❎ ERROR] 권한이 없는 사용자가 접근했습니다."),
