b6446850995d3f52cec86b9b31665bdaad2313ab
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-30 01:25:36 +0900

    refactor: AuthMember 도메인 매개변수 수정

--- src/main/java/com/authplayground/api/domain/member/model/AuthMember.java
+++ src/main/java/com/authplayground/api/domain/member/model/AuthMember.java
@@ -2,9 +2,9 @@ package com.authplayground.api.domain.member.model;
 
 import com.authplayground.api.domain.member.entity.Member;
 
-public record AuthMember(Long id, String email, Role role) {
+public record AuthMember(String email, String nickname, Role role) {
 
 	public static AuthMember from(Member member) {
-		return new AuthMember(member.getId(), member.getEmail(), member.getRole());
+		return new AuthMember(member.getEmail(), member.getNickname(), member.getRole());
 	}
 }
