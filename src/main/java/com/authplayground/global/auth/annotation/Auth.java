633b5f3512b67d4e9565a6f71ffaa2761d7cdc1d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-30 23:30:03 +0900

    feat: 익명 사용자 처리 안정성 확보

--- src/main/java/com/authplayground/global/auth/annotation/Auth.java
+++ src/main/java/com/authplayground/global/auth/annotation/Auth.java
@@ -6,8 +6,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : #this")
 public @interface Auth {
 }
