c37a41beb4f0166128a6f5a70a2585d27b2c4aa8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-03-30 19:44:52 +0900

    feat: AccessToken 검증 및 저장 필터 구현

--- src/main/java/com/authplayground/global/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/authplayground/global/auth/filter/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.authplayground.global.auth.filter;
 
 import static com.authplayground.global.common.util.JwtConstant.*;
 
-import java.io.IOException;
 import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,12 +9,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.authplayground.api.domain.member.model.AuthMember;
 import com.authplayground.global.auth.token.JwtProvider;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -26,28 +25,33 @@ import lombok.extern.slf4j.Slf4j;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtProvider jwtProvider;
+	private final HandlerExceptionResolver handlerExceptionResolver;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest httpServletRequest,
 		HttpServletResponse httpServletResponse,
-		FilterChain filterChain) throws ServletException, IOException {
+		FilterChain filterChain) {
 
 		try {
 			final String accessToken = jwtProvider.extractToken(httpServletRequest, AUTHORIZATION_HEADER);
 
 			if (jwtProvider.validateToken(accessToken)) {
-				final AuthMember authMember = jwtProvider.extractAuthMemberFromToken(accessToken);
-				final Authentication authentication = new UsernamePasswordAuthenticationToken(authMember, null,
-					List.of(new SimpleGrantedAuthority("ROLE_" + authMember.role().name())));
-
-				SecurityContextHolder.getContext().setAuthentication(authentication);
-
+				setAuthenticationContext(accessToken);
 				log.debug("[✅ LOGGER: JWT AUTHENTICATION FILTER] 인증 객체 등록이 완료되었습니다.");
 			}
+
+			filterChain.doFilter(httpServletRequest, httpServletResponse);
 		} catch (Exception e) {
 			log.warn("[✅ LOGGER: JWT AUTHENTICATION FILTER] 인증 실패 또는 토큰이 존재하지 않습니다: {}", e.getMessage());
+			handlerExceptionResolver.resolveException(httpServletRequest, httpServletResponse, null, e);
 		}
+	}
 
-		filterChain.doFilter(httpServletRequest, httpServletResponse);
+	private void setAuthenticationContext(String accessToken) {
+		final AuthMember authMember = jwtProvider.extractAuthMemberFromToken(accessToken);
+		final List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(authMember.role().name()));
+		final Authentication authentication = new UsernamePasswordAuthenticationToken(authMember, null, authorities);
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
 	}
 }
