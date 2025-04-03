1348716d410b60a1ea698fe07b7f4db327fec716
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-03 18:58:52 +0900

    test: 사용자 정보 상수 정의 및 Member 객체 생성 정적 팩토리 메서드 구현

---
+++ src/test/java/com/authplayground/support/MemberFixture.java
@@ -0,0 +1,28 @@
+package com.authplayground.support;
+
+import com.authplayground.api.domain.member.entity.Member;
+import com.authplayground.api.dto.member.request.SignUpRequest;
+
+public class MemberFixture {
+
+	public static final String EMAIL = "member@test.com";
+	public static final String PASSWORD = "test123!";
+	public static final String NICKNAME = "memberTest";
+	public static final String REGISTRATION_NUMBER = "980521-1234567";
+	public static final String ADDRESS = "경기도 구리시 인창동 123";
+
+	public static Member createMember() {
+		return Member.createMember(createSignUpRequest(), PASSWORD, REGISTRATION_NUMBER);
+	}
+
+	public static SignUpRequest createSignUpRequest() {
+		return SignUpRequest.builder()
+			.email(EMAIL)
+			.password(PASSWORD)
+			.passwordCheck(PASSWORD)
+			.nickname(NICKNAME)
+			.registrationNumber(REGISTRATION_NUMBER)
+			.address(ADDRESS)
+			.build();
+	}
+}
