6ba65bb6fbeaa5c0fff8258f04db3145781386f4
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-05 14:16:31 +0900

    test: 로그인 요청 DTO 생성 정적 팩토리 메서드 구현

--- src/test/java/com/authplayground/support/MemberFixture.java
+++ src/test/java/com/authplayground/support/MemberFixture.java
@@ -3,6 +3,7 @@ package com.authplayground.support;
 import com.authplayground.api.domain.member.entity.Member;
 import com.authplayground.api.domain.member.model.AuthMember;
 import com.authplayground.api.domain.member.model.Role;
+import com.authplayground.api.dto.auth.request.LoginRequest;
 import com.authplayground.api.dto.member.request.SignUpRequest;
 
 public class MemberFixture {
@@ -13,9 +14,21 @@ public class MemberFixture {
 	public static final String REGISTRATION_NUMBER = "980521-1234567";
 	public static final String ADDRESS = "경기도 구리시 인창동 123";
 
+	// ==============================================================
+	// ✅ 도메인 객체 생성
+	// ==============================================================
+
 	public static Member createMember() {
 		return Member.createMember(createSignUpRequest(), PASSWORD, REGISTRATION_NUMBER);
 	}
+
+	public static AuthMember createAuthMember() {
+		return new AuthMember(EMAIL, NICKNAME, Role.MEMBER);
+	}
+
+	// ==============================================================
+	// ✅ 요청 객체 생성 - 회원가입(SignUp)
+	// ==============================================================
 
 	public static SignUpRequest createSignUpRequest() {
 		return SignUpRequest.builder()
@@ -28,7 +41,28 @@ public class MemberFixture {
 			.build();
 	}
 
-	public static AuthMember createAuthMember() {
-		return new AuthMember(EMAIL, NICKNAME, Role.MEMBER);
+	// ==============================================================
+	// ✅ 요청 객체 생성 - 로그인(Login)
+	// ==============================================================
+
+	public static LoginRequest createLoginRequest() {
+		return LoginRequest.builder()
+			.email(EMAIL)
+			.password(PASSWORD)
+			.build();
+	}
+
+	public static LoginRequest createWrongEmailLoginRequest() {
+		return LoginRequest.builder()
+			.email("notfound@test.com")
+			.password(PASSWORD)
+			.build();
+	}
+
+	public static LoginRequest createWrongPasswordLoginRequest() {
+		return LoginRequest.builder()
+			.email(EMAIL)
+			.password("wrong-password")
+			.build();
 	}
 }
