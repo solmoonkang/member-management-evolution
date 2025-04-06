63bb1b61b8536852a9b8d0f1412696c50ec06397
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-06 21:54:36 +0900

    test: 인증 관련 핼퍼 로직 정의

---
+++ src/test/java/com/authplayground/support/helper/AuthTestHelper.java
@@ -0,0 +1,26 @@
+package com.authplayground.support.helper;
+
+import static com.authplayground.support.TestConstant.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.authplayground.api.dto.auth.request.LoginRequest;
+import com.authplayground.api.dto.member.request.SignUpRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AuthTestHelper {
+
+	public static ResultActions performSignup(MockMvc mockMvc, ObjectMapper objectMapper, SignUpRequest request) throws Exception {
+		return mockMvc.perform(post(SIGNUP_URL)
+			.contentType(JSON)
+			.content(objectMapper.writeValueAsString(request)));
+	}
+
+	public static ResultActions performLogin(MockMvc mockMvc, ObjectMapper objectMapper, LoginRequest request) throws Exception {
+		return mockMvc.perform(post(LOGIN_URL)
+			.contentType(JSON)
+			.content(objectMapper.writeValueAsString(request)));
+	}
+}
