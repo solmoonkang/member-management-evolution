2d63d435d483cd603a06ccb90e8bb68739b7dfe3
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-07 16:40:27 +0900

    test: 테스트 관련 상수 정의

--- src/test/java/com/authplayground/support/TestConstant.java
+++ src/test/java/com/authplayground/support/TestConstant.java
@@ -6,6 +6,8 @@ public class TestConstant {
 
 	public static final String SIGNUP_URL = "/api/members/signup";
 	public static final String LOGIN_URL = "/api/auth/login";
+	public static final String LOGOUT_URL = "/api/auth/logout";
+	public static final String REISSUE_URL = "/api/auth/reissue";
 	public static final MediaType JSON = MediaType.APPLICATION_JSON;
 
 	public static final String EMAIL = "member@test.com";
@@ -13,4 +15,6 @@ public class TestConstant {
 	public static final String NICKNAME = "memberTest";
 	public static final String REGISTRATION_NUMBER = "980521-1234567";
 	public static final String ADDRESS = "경기도 구리시 인창동 123";
+
+	public static final String BEARER_TYPE = "Bearer ";
 }
