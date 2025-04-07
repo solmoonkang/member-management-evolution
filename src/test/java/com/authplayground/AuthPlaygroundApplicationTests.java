15309a8dcd462774a6435e85dca2625e7e67f0ba
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-07 23:03:04 +0900

    test: Application 테스트 코드 구현

--- src/test/java/com/authplayground/AuthPlaygroundApplicationTests.java
+++ src/test/java/com/authplayground/AuthPlaygroundApplicationTests.java
@@ -1,13 +1,18 @@
 package com.authplayground;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class AuthPlaygroundApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	@DisplayName("[✅ SUCCESS] contextLoads - Spring Boot 애플리케이션 컨텍스트가 성공적으로 로드되었습니다.")
+	void contextLoads_success() {
 
+	}
 }
