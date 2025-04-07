5f003b68f1cf787e99bb7d34ce3d47d04e273f8f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-07 17:38:29 +0900

    test: 사용자 정보 조회 핼퍼 로직 구현

--- src/test/java/com/authplayground/support/helper/MemberTestHelper.java
+++ src/test/java/com/authplayground/support/helper/MemberTestHelper.java
@@ -3,17 +3,28 @@ package com.authplayground.support.helper;
 import static com.authplayground.support.TestConstant.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.authplayground.api.dto.member.request.SignUpRequest;
+import com.authplayground.api.dto.token.response.TokenResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class MemberTestHelper {
 
-	public static ResultActions performSignup(MockMvc mockMvc, ObjectMapper objectMapper, SignUpRequest request) throws Exception {
+	public static ResultActions performSignUp(MockMvc mockMvc, ObjectMapper objectMapper, SignUpRequest request) throws Exception {
 		return mockMvc.perform(post(SIGNUP_URL)
 			.contentType(JSON)
 			.content(objectMapper.writeValueAsString(request)));
+	}
+
+	public static ResultActions performFind(MockMvc mockMvc, TokenResponse tokenResponse) throws Exception {
+		return mockMvc.perform(get(FIND_URL)
+			.header(HttpHeaders.AUTHORIZATION, BEARER_TYPE + tokenResponse.accessToken()));
+	}
+
+	public static ResultActions performFind(MockMvc mockMvc) throws Exception {
+		return mockMvc.perform(get(FIND_URL));
 	}
 }
