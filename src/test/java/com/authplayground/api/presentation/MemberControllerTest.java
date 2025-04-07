5327e398e138a7f2296455bc5825111ce9b21bce
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-07 17:38:59 +0900

    test: 사용자 정보 조회 테스트 코드 구현

--- src/test/java/com/authplayground/api/presentation/MemberControllerTest.java
+++ src/test/java/com/authplayground/api/presentation/MemberControllerTest.java
@@ -1,6 +1,5 @@
 package com.authplayground.api.presentation;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -9,10 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.authplayground.api.dto.auth.request.LoginRequest;
 import com.authplayground.api.dto.member.request.SignUpRequest;
+import com.authplayground.api.dto.token.response.TokenResponse;
 import com.authplayground.support.fixture.MemberFixture;
+import com.authplayground.support.helper.AuthTestHelper;
 import com.authplayground.support.helper.MemberTestHelper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -35,7 +38,7 @@ class MemberControllerTest {
 		SignUpRequest signUpRequest = MemberFixture.createSignUpRequest();
 
 		// WHEN & THEN
-		MemberTestHelper.performSignup(mockMvc, objectMapper, signUpRequest)
+		MemberTestHelper.performSignUp(mockMvc, objectMapper, signUpRequest)
 			.andExpect(status().isOk())
 			.andExpect(content().string("[✅ SUCCESS] 사용자 정보를 성공적으로 생성했습니다."));
 	}
@@ -47,7 +50,7 @@ class MemberControllerTest {
 		SignUpRequest wrongSignUpRequest = MemberFixture.createSignUpRequestWithWrongEmail();
 
 		// WHEN & THEN
-		MemberTestHelper.performSignup(mockMvc, objectMapper, wrongSignUpRequest)
+		MemberTestHelper.performSignUp(mockMvc, objectMapper, wrongSignUpRequest)
 			.andExpect(status().isBadRequest());
 	}
 
@@ -58,7 +61,39 @@ class MemberControllerTest {
 		SignUpRequest wrongSignUpRequest = MemberFixture.createSignUpRequestWithWrongPasswordCheck();
 
 		// WHEN & THEN
-		MemberTestHelper.performSignup(mockMvc, objectMapper, wrongSignUpRequest)
+		MemberTestHelper.performSignUp(mockMvc, objectMapper, wrongSignUpRequest)
 			.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	@DisplayName("[✅ SUCCESS] findMemberInfo - 로그인 사용자의 정보를 성공적으로 조회했습니다.")
+	void findMemberInfo_returnsMemberInfoResponse_success() throws Exception {
+		// GIVEN
+		SignUpRequest signUpRequest = MemberFixture.createSignUpRequest();
+		LoginRequest loginRequest = MemberFixture.createLoginRequest();
+
+		MemberTestHelper.performSignUp(mockMvc, objectMapper, signUpRequest);
+		MvcResult loginResult = AuthTestHelper.performLogin(mockMvc, objectMapper, loginRequest)
+			.andReturn();
+
+		String content = loginResult.getResponse().getContentAsString();
+		TokenResponse tokenResponse = objectMapper.readValue(content, TokenResponse.class);
+
+		// WHEN & THEN
+		MemberTestHelper.performFind(mockMvc, tokenResponse)
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.email").value(signUpRequest.email()))
+			.andExpect(jsonPath("$.nickname").value(signUpRequest.nickname()))
+			.andExpect(jsonPath("$.address").value(signUpRequest.address()));
+	}
+
+	@Test
+	@DisplayName("[❎ FAILURE] findMemberInfo - 인증 없이 요청하여 사용자의 정보 조회에 실패했습니다.")
+	void findMemberInfo_throwsUnauthorizedException_whenTokenMissing_failure() throws Exception {
+		// GIVEN
+
+		// WHEN & THEN
+		MemberTestHelper.performFind(mockMvc)
+			.andExpect(status().isUnauthorized());
 	}
 }
