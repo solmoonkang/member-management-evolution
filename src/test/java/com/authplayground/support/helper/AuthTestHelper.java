5f2a4309edb19ff4a39cd74ebbb161c0d8650b6d
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-18 15:54:42 +0900

    test: 토큰 응답과 실패 메시지 응답 로직 구현

--- src/test/java/com/authplayground/support/helper/AuthTestHelper.java
+++ src/test/java/com/authplayground/support/helper/AuthTestHelper.java
@@ -1,10 +1,13 @@
 package com.authplayground.support.helper;
 
 import static com.authplayground.global.common.util.JwtConstant.*;
-import static com.authplayground.support.TestConstant.*;
 import static com.authplayground.support.TestConstant.BEARER_TYPE;
+import static com.authplayground.support.TestConstant.REFRESH_TOKEN_HEADER;
+import static com.authplayground.support.TestConstant.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.hamcrest.Matchers;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -45,5 +48,27 @@ public class AuthTestHelper {
 		return mockMvc.perform(post(REISSUE_URL)
 			.header(HttpHeaders.AUTHORIZATION, BEARER_TYPE + accessToken)
 			.header(REFRESH_TOKEN_HEADER, refreshToken));
+	}
+
+	public static void expectTokenResponse(ResultActions resultActions) throws Exception {
+		resultActions
+			.andExpect(jsonPath("$.accessToken").exists())
+			.andExpect(jsonPath("$.refreshToken").exists());
+	}
+
+	public static void expectFailureMessage(ResultActions resultActions, String message) throws Exception {
+		resultActions.andExpect(jsonPath("$.message").value(message));
+	}
+
+	public static void expectRefreshTokenCreated(ResultActions resultActions, String refreshToken) throws Exception {
+		resultActions
+			.andExpect(header().string(HttpHeaders.SET_COOKIE, Matchers.containsString("Refresh-Token=" + refreshToken)))
+			.andExpect(header().string(HttpHeaders.SET_COOKIE, Matchers.not(Matchers.containsString("Max-Age=0"))));
+	}
+
+	public static void expectRefreshTokenDeleted(ResultActions resultActions) throws Exception {
+		resultActions
+			.andExpect(header().string(HttpHeaders.SET_COOKIE, Matchers.containsString("Refresh-Token=;")))
+			.andExpect(header().string(HttpHeaders.SET_COOKIE, Matchers.containsString("Max-Age=0")));
 	}
 }
