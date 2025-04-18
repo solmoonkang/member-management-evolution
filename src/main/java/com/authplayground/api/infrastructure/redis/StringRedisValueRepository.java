05ed7b34bf10c42f1852d51c2f9eeaaaad3dc69f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2025-04-18 15:18:12 +0900

    refactor: 만료 시간 데이터 타입 수정

--- src/main/java/com/authplayground/api/infrastructure/redis/StringRedisValueRepository.java
+++ src/main/java/com/authplayground/api/infrastructure/redis/StringRedisValueRepository.java
@@ -1,6 +1,6 @@
 package com.authplayground.api.infrastructure.redis;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,8 +13,8 @@ public class StringRedisValueRepository {
 
 	private final StringRedisTemplate stringRedisTemplate;
 
-	public void save(String key, String value, Duration expired) {
-		stringRedisTemplate.opsForValue().set(key, value, expired);
+	public void save(String key, String value, long expiration) {
+		stringRedisTemplate.opsForValue().set(key, value, expiration, TimeUnit.MILLISECONDS);
 	}
 
 	public String find(String key) {
